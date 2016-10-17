0b4414412a05a890795211e90fccbb834c1c268a
Razvan-Gabriel Geangu <k1502791@kcl.ac.uk>
2016-10-17 16:41:25 +0100

    added method to identify the type description of a square (free, bomb, undisclosed or calculated number)

--- MinesweeperFX/src/razvangeangu/view/CustomSquare.java
+++ MinesweeperFX/src/razvangeangu/view/CustomSquare.java
@@ -9,23 +9,47 @@ public class CustomSquare extends Pane {
 	private int type, row, column;
 	private ImageView icon;
 	private boolean hasFlag;
+	private String typeDescription;
 
 	public CustomSquare() {
 		maxWidth(20);
 		maxHeight(20);
 
 		hasFlag = false;
+		typeDescription = "undisclosed";
 		
 		icon = new ImageView();
 		setIcon(0);
 		getChildren().add(icon);
-	
 	}
 	
 	public void setIcon(int type) {
 		icon.setImage(new Image("razvangeangu/resources/Minesweeper_" + type + ".png"));
 		icon.setFitWidth(20);
         icon.setPreserveRatio(true);
+        
+        switch (type) {
+	        case -2: {
+	        	typeDescription = "free";
+	        	break;
+	        }
+	        case -1: {
+	        	typeDescription = "bomb";
+	        	break;
+	        }
+	        case 0: {
+	        	typeDescription = "undisclosed";
+	        	break;
+	        }
+	        case 9: {
+	        	typeDescription = "flag";
+	        	break;
+	        }
+	        default: {
+	        	typeDescription = Integer.toString(type);
+	        	break;
+	        }
+        }
 	}
 	
 	public void setType(int type) {
@@ -64,5 +88,7 @@ public class CustomSquare extends Pane {
 		this.hasFlag = hasFlag;
 	}
 	
-	
+	public String getTypeDescription() {
+		return typeDescription;
+	}
 }
